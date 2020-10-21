e767f3d46854dce8fc6efc0e07c8609b67ecb27c
Castill4lejandro <72233724+Castill4lejandro@users.noreply.github.com>
2020-10-21 15:46:56 -0500

    Add files via upload

---
+++ EntradaDatos/EntradaDatos.java
@@ -0,0 +1,8 @@
+class EntradaDatos{
+   public static void main(String [] args){
+       String nombre;
+       System.out.println("Dime tu nombre uwu");
+       nombre = System.console().readLine();
+       System.out.println("Bienvenido:" + nombre + " a programacion");
+   } 
+}
