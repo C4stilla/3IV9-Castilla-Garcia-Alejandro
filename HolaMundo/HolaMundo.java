446ae523f79261cf992a7d9f9376451e15567a01
Castill4lejandro <72233724+Castill4lejandro@users.noreply.github.com>
2020-10-21 15:45:01 -0500

    Add files via upload
    ES UN PINCHE HOLA MUNDO

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,7 @@
+class HolaMundo{
+     public static void main(String[] args){
+        String nombre = "Cosa Kawaii";
+        System.out.println("Porfis di hola mundo");
+        System.out.print(nombre);
+     }
+}
